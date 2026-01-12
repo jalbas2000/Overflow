72d583d8a68815419629d2b19113576d26d89ddb
Jalal Basha <jalbas2000@yahoo.com>
2026-01-12 23:06:57 +0400

    End of Section 5

--- QuestionService/Data/QuestionDbContext.cs
+++ QuestionService/Data/QuestionDbContext.cs
@@ -6,6 +6,7 @@ namespace QuestionService.Data;
 public class QuestionDbContext(DbContextOptions options) : DbContext(options)
 {
     public DbSet<Question>  Questions { get; set; }
+    public DbSet<Answer>  Answers { get; set; }
     public DbSet<Tag> Tags { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
